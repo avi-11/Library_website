9a5dfa8de0c18ad3f07e80a120c90bdefebe6f0b
avi-11 <avi1109cool@gmail.com>
2021-07-15 21:32:53 +0530

    --prb--

--- work.js
+++ work.js
@@ -126,9 +126,14 @@ class Book {
     // Remove book from UI
     UI.deleteBook(e.target);
   
-    // Remove book from store
-    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+   
   
-    // Show success message
+    if(e.target.classList.contains("delete")){
+
+       // Remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+      // Show success message
     UI.showAlert('Book Removed', 'success');
+    }
   });
+
